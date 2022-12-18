356a2efa08b403e1ef0c3d16338f8cdee0159b1f
Troy Hamilton <110515824+hamiltontroy@users.noreply.github.com>
2022-12-17 22:09:50 -0600

    Update cppserver.cpp

--- cppserver.cpp
+++ cppserver.cpp
@@ -9,8 +9,6 @@ int main(void)
 {
     Ttcp
         instance;
-    struct ttcp::MessageInfo
-        messageInfo;
     const char
         *helloMsg = "Hello Client from Server C++ program.\n";
         
@@ -18,19 +16,11 @@ int main(void)
 
     instance.waitForAClient();
     
-    messageInfo = instance.receiveMessage(1234); // receives message into a dynamically allocated buffer
+    instance.receiveMessage(4096); // receives message into a dynamically allocated buffer
     
     instance.printMessage();
     
     send(instance.clientfd(), helloMsg, strlen(helloMsg), 0);
-    
-    
-    
-    //conn.printMessage(); // prints a message to stdout, skips unprintable characters. use verbosePrintMessage to print full info about the message.
-    //conn.deallocateMessage(); // if a message is not deallocated, a memory leak can occur. works with free();
-    //conn.transmitMessage("Hello client! This is Troy!\n"); // shows const char* here, works with std::string as well.
-
-    
     
     return 0;
 }
